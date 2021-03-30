65218ddb8d9c4fc249a9269559e1bfd253a73a35
madtek@gmx.net <gregor@sklorz.net>
2021-03-30 17:04:52 +0200

    copy test

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
-console.log("blub");
-console.log("args", arguments);
-console.log("process", process);
-console.log("process.argv", process.argv);
-console.log("args", args);
+let fs = require("fs");
+
+// console.log("blub");
+// console.log("args", arguments);
+// console.log("process", process);
+console.log("process.argv", process.argv, __dirname);
+
+fs.readFile( process.argv[2], (err, data) => {
+    if(err) {
+        throw err;
+    }
+    console.log(data.toString());
+
+    fs.writeFile("result", data, (er, dat) => {
+        if(er) {
+            throw er;
+        }
+        console.log("SAVE result file DONE.");
+    });
+});
