f399441805d697789f0f75ef46cb13f5b96e1ec9
madtek@gmx.net <gregor@sklorz.net>
2021-03-30 17:48:37 +0200

    parsing nls dirty

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
 #!/usr/bin/env node
 
-let fs = require("fs");
+//Most awesome dirty hack script
 
-// console.log("blub");
-// console.log("args", arguments);
-// console.log("process", process);
+let fs = require("fs");
+let readline = require("readline");
+
+
 console.log("process.argv", process.argv, __dirname);
 
 fs.readFile( process.argv[2], (err, data) => {
     if(err) {
         throw err;
     }
-    console.log(data.toString());
+    // console.log(data.toString());
 
-    fs.writeFile("result", data, (er, dat) => {
+    const rl = readline.createInterface({
+        input: data
+    });
+
+    let result = {de:{}, en:{}, fr:{}, it:{}};
+    let line = "";
+    let idx = 0;
+    let ns = "";
+    let entries = [];
+    rl.on("line", (input) => {
+        line = input.toString();
+
+        if(line.indexOf("<") >= 0) {
+            return;
+        }
+
+        //ist eine .htm zeile
+        idx = line.indexOf(".htm");
+        if(idx > 0) {
+            ns = line.slice(0, idx);
+
+            if(!result.de[ns]) {
+                result.de[ns] = {};
+                result.en[ns] = {};
+                result.fr[ns] = {};
+                result.it[ns] = {};
+            }
+
+            //have enough entries for all
+            entries = line.split("\t");
+            if(entries.length >= 6) {
+                result.de[ns][entries[1]] = entries[4];
+                result.en[ns][entries[1]] = entries[4];
+                result.fr[ns][entries[1]] = entries[5];
+                result.it[ns][entries[1]] = entries[6];
+            }
+        }
+    });
+
+    fs.writeFile("result", result.toString(), (er, dat) => {
         if(er) {
             throw er;
         }
